8473903059af8112081901039335c2e2b597480a
Shihyun Yu <tlgusyu@kyonggi.ac.kr>
2025-04-29 21:52:59 +0900

    fix: 로그인 충돌 해결 및 코드 정리

--- app/src/main/java/com/example/gildonaitemp/LoginActivity.java
+++ app/src/main/java/com/example/gildonaitemp/LoginActivity.java
@@ -5,18 +5,11 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
+import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.textfield.TextInputEditText;
 
 public class LoginActivity extends AppCompatActivity {
-
-    private TextInputEditText idText;
-    private TextInputEditText passwordText;
     private EditText etUsername, etPassword;
     private Button loginButton;
 
@@ -25,38 +18,22 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        idText = findViewById(R.id.etEmail);
-        passwordEditText = findViewById(R.id.etPassword);
-        loginButton = findViewById(R.id.btnLogin);
-
-    }
-
         etUsername = findViewById(R.id.ID);
         etPassword = findViewById(R.id.PW);
         loginButton = findViewById(R.id.login_button);
 
         loginButton.setOnClickListener(v -> {
-//            String username = etUsername.getText().toString();
-//            String password = etPassword.getText().toString();
-//
-//            if (username.isEmpty() || password.isEmpty()) {
-//                Toast.makeText(LoginActivity.this, "아이디와 비밀번호를 입력하세요", Toast.LENGTH_SHORT).show();
-//            } else {
-//                // 서버로 로그인 요청
-//                loginUser(username, password);
-//            }
+            String username = etUsername.getText().toString();
+            String password = etPassword.getText().toString();
 
-            //임시
-            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-            startActivity(intent);
+            if (username.isEmpty() || password.isEmpty()) {
+                Toast.makeText(LoginActivity.this, "아이디와 비밀번호를 입력하세요", Toast.LENGTH_SHORT).show();
+            } else {
+                // 임시
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
 
+            }
         });
-
-
-
     }
-
-    private void loginUser(String username, String password) {
-    }
-
 }
